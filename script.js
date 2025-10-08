402e1bd6d38d41c423e0adbe386a5ce16eae2613
KaissoGithub <Ahmedkaissoumi@gmail.com>
2025-10-08 17:47:09 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  
+  });
+  
+  
+  
+  
+  
