1ea24d41dc844f7024400cf454d3e7907185f716
201813880 <143803412+KaissoGithub@users.noreply.github.com>
2025-10-26 22:57:01 -0400

    pre-github-page
    Ajouts de commentaires, suppression coquilles vides, ajout de texte, ajout de responsive pour mini galerie

--- script.js
+++ script.js
@@ -22,26 +22,26 @@ createApp({
 
   methods: {
     inverse() {
-      this.flipped = !this.flipped;
+      this.flipped = !this.flipped; //on inverse la valeur de this.flipped, si elle est true, elle est false, si elle est false, elle est true
     },
-    toggleDark() {
-      this.dark = !this.dark;
-      document.body.classList.toggle('dark', this.dark);
-
+    toggleSombre() {
+      this.dark = !this.dark; //si true, false, et inversement
+      document.body.classList.toggle('dark', this.dark); //on applique ou retire this.dark
+      //dans le body, on ajoute ou retire dark en fonction de son toggle
       if (this.dark) {
         fond = "./assets/img/backintro_dark.png";
       } else {
         fond = "./assets/img/backIntro2.png";
       }
 
-      document.body.style.backgroundImage = `url('${fond}')`;
+      document.body.style.backgroundImage = `url('${fond}')`; //on applique la variable fond qui en réalité change en fonction de si elle est dark ou non
     }
   },
 }).mount('#app');
 
 
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () { //on attend que le dom soit chargé
   var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
@@ -69,8 +69,8 @@ ScrollTrigger.create({
       document.body.style.backgroundImage = "url('./assets/img/gris.png')";
     }
   },
-  onEnterBack: () => { // code exécuté lorsqu'on sort de la section, je l'ai mis pour être sûr de passer au background light et éviter les bugs
-    if (!document.body.classList.contains("dark")) {
+  onEnterBack: () => { // code exécuté lorsqu'on RETOURNE dans la section, je l'ai mis pour être sûr de passer au background light et éviter les bugs
+    if (!document.body.classList.contains("dark")) { //si ne contient pas dark..
       document.body.style.backgroundImage = "url('./assets/img/gris.png')";
     }
   }
@@ -86,8 +86,8 @@ ScrollTrigger.create({
       document.body.style.backgroundImage = "url('./assets/img/rouge.png')";
     }
   },
-  onEnterBack: () => {
-    if (!document.body.classList.contains("dark")) {  // code exécuté lorsqu'on sort de la section, je l'ai mis pour être sûr de passer au background light et éviter les bugs
+  onEnterBack: () => { // code exécuté lorsqu'on RETOURNE dans la section
+    if (!document.body.classList.contains("dark")) {  // code exécuté lorsqu'on RETOURNE dans la section, je l'ai mis pour être sûr de passer au background light et éviter les bugs
       document.body.style.backgroundImage = "url('./assets/img/rouge.png')";
     }
   }
@@ -103,8 +103,8 @@ ScrollTrigger.create({
       document.body.style.backgroundImage = "url('./assets/img/violet.png')";
     }
   },
-  onEnterBack: () => {
-    if (!document.body.classList.contains("dark")) {  // code exécuté lorsqu'on sort de la section, je l'ai mis pour être sûr de passer au background light et éviter les bugs
+  onEnterBack: () => { // code exécuté lorsqu'on RETOURNE dans la section
+    if (!document.body.classList.contains("dark")) {  // code exécuté lorsqu'on RETOURNE dans la section, je l'ai mis pour être sûr de passer au background light et éviter les bugs
       document.body.style.backgroundImage = "url('./assets/img/violet.png')";
     }
   }
@@ -120,8 +120,8 @@ ScrollTrigger.create({
       document.body.style.backgroundImage = "url('./assets/img/vert.png')";
     }
   },
-  onEnterBack: () => {
-    if (!document.body.classList.contains("dark")) {  // code exécuté lorsqu'on sort de la section, je l'ai mis pour être sûr de passer au background light et éviter les bugs
+  onEnterBack: () => { // code exécuté lorsqu'on RETOURNE dans la section
+    if (!document.body.classList.contains("dark")) { // code exécuté lorsqu'on RETOURNE dans la section, je l'ai mis pour être sûr de passer au background light et éviter les bugs
       document.body.style.backgroundImage = "url('./assets/img/vert.png')";
     }
   }
